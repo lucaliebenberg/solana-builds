106dbe018513a56f0a5eabcdc0b34c11cfceb307
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-28 21:25:19 +0200

    added main func, implementing main contract

--- counter/counter/src/lib.rs
+++ counter/counter/src/lib.rs
@@ -1,5 +1,10 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 // use borsh::{BorshDeserialize, BorshSerialize};
-use borsh_derive::{BorshDeserialize, BorshSerialize};
+// use borsh_derive::{BorshDeserialize, BorshSerialize};
+use instructions::{
+    CounterInstructions, 
+    // UpdateArgs
+};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
@@ -13,4 +18,41 @@ pub mod instructions;
 #[derive(Debug, BorshDeserialize, BorshSerialize)]
 pub struct CounterAccount {
     pub counter: u32
+}
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instructions_data: &[u8],
+) -> ProgramResult {
+
+    msg!("Counter program entry point");
+
+    let instructions: CounterInstructions = CounterInstructions::unpack(instructions_data)?;
+
+    let accounts_iter = &mut accounts.iter();
+    let account = next_account_info(accounts_iter)?;
+
+    let mut counter_account = CounterAccount::try_from_slice(&account.data.borrow())?;
+
+    match instructions {
+        CounterInstructions::Increment => {
+            counter_account.counter += 1;
+        }
+        CounterInstructions::Decrement => {
+            counter_account.counter -= 1;
+        }
+        CounterInstructions::Reset => {
+            counter_account.counter = 0;
+        }
+        CounterInstructions::Update(args) => {
+            counter_account.counter = args.value;
+        }
+    }
+
+    counter_account.serialize(&mut &mut account.data.borrow_mut() [..])?;
+    Ok(())
+
 }
