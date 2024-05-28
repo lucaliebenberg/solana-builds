15276f1320060b59c33a65e1023799e1c25fcd7d
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-28 20:58:38 +0200

    project instr update

--- counter/counter/src/instructions.rs
+++ counter/counter/src/instructions.rs
@@ -2,9 +2,15 @@ use borsh::BorshDeserialize;
 use borsh_derive::{BorshDeserialize, BorshSerialize};
 use solana_program::program_error::ProgramError;
 
+#[derive(Debug, BorshDeserialize, BorshSerialize)]
+pub struct  UpdateArgs {
+    pub value: u32
+}
+
 pub enum CounterInstructions {
     Increment,
     Decrement,
-    Update,
+    Update(UpdateArgs),
     Reset,
 }
+
